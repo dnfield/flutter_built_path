1ce3bdcee07c76841bea6efec82b5900bd3ce8c7
Dan Field <dnfield@travelers.com>
2018-08-02 10:18:17 -0400

    cleanup/format

--- built_path_annotations/lib/built_path_annotations.dart
+++ built_path_annotations/lib/built_path_annotations.dart
@@ -1,15 +1,11 @@
-// import 'dart:ui' show PathFillType;
-
 class SvgPath {
   const SvgPath(this.data, {this.fillRule});
 
   /// The SVG Path data string, i.e. the `d` attribute from an SVG `<path>` element.
   final String data;
-  /// The [PathFillType] for the [Path].
+
+  /// Will control the [PathFillType] for the [Path].
   final FillRule fillRule;
 }
 
-enum FillRule {
-  nonZero,
-  evenOdd
-}
+enum FillRule { nonZero, evenOdd }
